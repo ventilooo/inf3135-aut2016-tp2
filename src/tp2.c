d74a26fe341ef35e0b63dd33d251aced37e3e625
NizarSemlali <nizarsemlali@hotmail.fr>
2016-11-21 19:24:12 -0500

    Implémentation de la méthode getIndexPays qui récupère l'index du pays dans le fichier Json

--- src/tp2.c
+++ src/tp2.c
@@ -6,15 +6,53 @@ bool SHOWLANGUAGES = false;
 bool SHOWCAPITAL = false;
 bool SHOWBORDERS = false;
 bool SHOWFLAG = false;
-char COUNTRY[3];
+char COUNTRY[4];
 char REGION[8];
+int indexPays ; 
+char *capitale ; 
+char *nomPays ; 
 
+void getOpts(int argc, char *argv[]) ; 
+void help() ; 
+int getIndexPays(json_t *objetJson, char* pays, int nombreTotalPays ) ; 
+char* getCapitale(json_t *objetJson, int indexPays) ; 
+char* getNomPays(json_t *objetJson, int indexPays) ;
 void getPays(bool showlanguages, bool showcapital, bool showflag, char country[], char region[] );
 
+
 int main(int argc, char *argv[]){
+
+    //Récupération des arguments
     getOpts(argc,argv);
+    
+    //Init objet Json du dossier data 
+    json_t *objetJson ; 
+    objetJson = json_load_file("../data/countries/countries.json", 0, NULL);
+    
+    // Récupération du nombre total de pays
+    int nombreTotalPays = json_array_size(objetJson);
+    printf("Nombre total pays : %d",nombreTotalPays);
+ 
+    // Récupération de l'index du pays en fonction du nom récupéré
+    indexPays = getIndexPays(objetJson,COUNTRY,nombreTotalPays) ; 
+    
+    // Récupération de la capitale du pays en question 
+    capitale = getCapitale(objetJson, indexPays) ; 
+    
+    // Récupération du nom du pays 
+    nomPays = getNomPays(objetJson, indexPays) ; 
+    
+    
+    printf(" Abréviation 3 lettres : %s \n", COUNTRY) ;
+    printf(" Index : %d \n" , indexPays) ; 
+    //printf(" Capitale : %s \n", capitale) ; 
+    //printf(" Nom Pays : %s \n", nomPays) ; 
+    
     return 0;
 }
+
+
+
 
 void getOpts(int argc, char *argv[]){
 
@@ -70,6 +108,7 @@ void getOpts(int argc, char *argv[]){
 }
 
 
+
 void help(){
     int c;
     FILE *fHelp;
@@ -87,28 +126,45 @@ void help(){
     }
 }
 
-void getPays(bool showlanguages, bool showcapital, bool showflag, char country[], char region[] ){
 
-    
+int getIndexPays(json_t *objetJson, char* pays, int nombreTotalPays) {
+
+	int indexPays = 0  ; 
+	json_t *paysCible ; 
+	json_t *ciocPays ; 
+	char *ciocCorrespondant ;
+	int i ; 
+	
+	
+	for ( i = 0 ; i < nombreTotalPays ; i++ ) {
+	
+		paysCible = json_array_get(objetJson, i);
+		ciocPays = json_object_get(paysCible,"cioc");
+		ciocCorrespondant = json_string_value(ciocPays) ;
+		if ( strcasecmp(pays, ciocCorrespondant) == 0 ) {
+			indexPays = i ;
+		}
+		
+	}
+	
+	return indexPays ; 
+	
+}
+
+char* getCapitale(json_t *objetJson, int indexPays) {
+	
+	return "";
 
 }
 
-void initJson(json_t *objetJson){
- 
-    objetJson = json_load_file("../data/countries/countries.json", 0, NULL);
-    /*
-    json_t *countries;
-    countries = fopen("../data/countries/countries.json", "r");
+char* getNomPays(json_t *objetJson, int indexPays) {
 
- if(countries){
-        
-        json_object_update(objetJson, countries); 
-        
-        fclose(countries);
-        
-    }else{
-        printf("I/0 Exception\n");
-        exit(1);
-    }
-*/
+	return ""; 
+	
 }
+
+
+void getPays(bool showlanguages, bool showcapital, bool showflag, char country[], char region[] ){   
+
+}
+
