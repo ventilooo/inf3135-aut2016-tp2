78b40404c8f2a2f3203041b5657138c2088415d7
iszalai <szalai972@gmail.com>
2016-11-21 23:52:12 -0500

    Multiple Module
    New link between module by link.h
    Json manipulation are in country.c
    
    Signed-off-by: iszalai <szalai972@gmail.com>
    
     Changes to be committed:
    	new file:   src/country.c
    	new file:   src/country.h
    	new file:   src/link.h
    	modified:   src/tp2.c
    	modified:   src/tp2.h

---
+++ src/country.c
@@ -0,0 +1,80 @@
+#include "country.h"
+
+int getIndexPays(json_t *objetJson, char* pays, int nombreTotalPays) {
+
+	int indexPays = 0; 
+	json_t *paysCible; 
+	json_t *ciocPays; 
+	char *ciocCorrespondant;
+	int i; 
+	
+	
+	for ( i = 0 ; i < nombreTotalPays ; i++ ) {
+	
+		paysCible = json_array_get(objetJson, i);
+		ciocPays = json_object_get(paysCible,"cioc");
+		ciocCorrespondant = json_string_value(ciocPays) ;
+		if ( strcasecmp(pays, ciocCorrespondant) == 0 ) {
+			indexPays = i;
+		}	
+	}
+	return indexPays; 
+}
+
+char* getCapitale(json_t *objetJson, int indexPays) {
+	
+	 json_t *paysCible;
+	 json_t *valeurCapitale; 
+	
+	 paysCible = json_array_get(objetJson, indexPays);
+     valeurCapitale = json_object_get(paysCible, "capital");
+     char *capitale = json_string_value(valeurCapitale);
+     
+     return capitale;
+}
+
+char* getNomPays(json_t *objetJson, int indexPays) {
+
+	json_t *paysCible;
+	json_t *nomsDuPays;
+	json_t *nomCommun;
+	
+	paysCible = json_array_get(objetJson, indexPays);
+	nomsDuPays = json_object_get(paysCible,"name");  
+	nomCommun = json_object_get(nomsDuPays,"common");
+	char *nomCommunPays = json_string_value(nomCommun);
+    
+    return nomCommunPays;
+}
+
+void printLangues (json_t *objetJson, int indexPays) {
+
+	json_t *paysCible; 
+	json_t *languesDuPays; 
+	const char *key;
+    	json_t *value;
+	
+	paysCible = json_array_get(objetJson, indexPays) ; 
+	languesDuPays = json_object_get(paysCible,"languages");
+	printf("Languages: ");
+	json_object_foreach(languesDuPays, key, value) {
+		printf("%s, ",json_string_value(value)); 
+	}
+	printf("\n");
+} 
+
+
+void affichage(bool SHOWCAPITAL, bool SHOWLANGUAGES, bool SHOWFLAG, bool SHOWBORDERS, char country[], char region[]){ 
+	
+	if ( SHOWCAPITAL ) {
+		
+	}  
+	
+	if ( SHOWLANGUAGES ) {
+		
+	}
+	
+	if ( SHOWBORDERS ) {
+	
+	}
+}
