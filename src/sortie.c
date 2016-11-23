631db41c4642423408235960c524cbb08cc5378d
Pascal Vautour <vautour.pascal@gmail.com>
2016-11-23 01:39:58 -0500

    Refractor de la variable pays pour indexPays par soucis de coherence avec coutry.c

--- src/sortie.c
+++ src/sortie.c
@@ -15,45 +15,44 @@ void closeGraphFile(FILE fGraph){
     fprintf(fGraph, "}");
     fclose(fGraph);
 }
-void addPays(FILE fGraph, int pays){
+void addPays(FILE fGraph, int indexPays){
 
     fprintf(fGraph, "%s [", pays);
     fprintf(fGraph, "shape = none,");
     fprintf(fGraph, " label = <<table border="0" cellspacing="0">");
     fprintf(fGraph, " <tr><td align=\"center\" border=\"1\" fixedsize=\"true\" width=\"200\" height=\"100\">
-            <img src=\"%s.png\" scale=\"true\"/></td></tr>", getPays(objetJson));
-    fprintf(fGraph," <tr><td align=\"left\" border=\"1\"><b>Name</b>: %s</td></tr>", getNomPays(objetJson, pays));
-    fprintf(fGraph, "<tr><td align=\"left\" border=\"1\"><b>Code</b>: %s</td></tr>", getCodePays(objetJson, pays));
-    capital(objetJson, pays, capital);
-    languages(fGraph, pays, languages);
-    Borders(fGraph, pays, borders);
+            <img src=\"%s.png\" scale=\"true\"/></td></tr>", getCodePays(objetJson, indexPays));
+    fprintf(fGraph," <tr><td align=\"left\" border=\"1\"><b>Name</b>: %s</td></tr>", getNomPays(objetJson, indexPays));
+    fprintf(fGraph, "<tr><td align=\"left\" border=\"1\"><b>Code</b>: %s</td></tr>", getCodePays(objetJson, indexPays));
+    capitalOut(objetJson, indexPays, capital);
+    languagesOut(fGraph, indexPays, languages);
+    BordersOut(fGraph, indexPays, borders);
     fprintf(fGraph, "  </table>>");
     fprintf(fGraph, " ];");
 
 }
 
-void languages(json_t *objetJson, FILE fGraph, int pays, bool languages){
+void languagesOut(json_t *objetJson, FILE fGraph, int indexPays, bool languages){
     char strLangues[MAXLENGTHLANGUES];
     if(languages){
-        fprintf(fGraph, "<tr><td align=\"left\" border=\"1\"><b>Language</b>: %s</td></tr>", getLanguage(objetJson, pays, strLangues));
+        fprintf(fGraph, "<tr><td align=\"left\" border=\"1\"><b>Language</b>: %s</td></tr>", getLanguage(objetJson, indexPays, strLangues));
     }
 }
 
-void capital(Fjson_t *objetJson, FILE fGraph, int pays, bool capital){
+void capitalOut(Fjson_t *objetJson, FILE fGraph, int indexPays, bool capital){
     if(capital){
-        fprintf(fGraph, "<tr><td align=\"left\" border=\"1\"><b>Capital</b>: %s</td></tr>", getCapital(objetJson, pays));
+        fprintf(fGraph, "<tr><td align=\"left\" border=\"1\"><b>Capital</b>: %s</td></tr>", getCapital(objetJson, indexPays));
     }
 }
 
-void borders(FILE fGraph, int pays, bool borders){
+void bordersOut(FILE fGraph, int indexPays, bool borders){
     char strBorders[MAXLENGTHBORDERS];
-
     if(borders){
-        fprintf(fGraph, "<tr><td align=\"left\" border="1"><b>Borders</b>: %s</td></tr>", getBorders(objetJson, pays, strBorders));
+        fprintf(fGraph, "<tr><td align=\"left\" border="1"><b>Borders</b>: %s</td></tr>", getBorders(objetJson, indexPays, strBorders));
     }
 }
 
-void frontieres(int pays){
+void frontieresOut(int indexPays){
 
 
 }
